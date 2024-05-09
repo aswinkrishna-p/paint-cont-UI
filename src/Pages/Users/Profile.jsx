5169c74faae1ab0b0694c8170071d2604c3294cc
aswin krishna <aswinkrishna33716@gmail.com>
2024-05-09 23:28:33 +0530

    Made changes in profile UI

--- src/Pages/Users/Profile.jsx
+++ src/Pages/Users/Profile.jsx
@@ -1,60 +1,142 @@
-import React, { useEffect } from 'react';
+import React, { useEffect ,useState} from 'react';
 import ClientNav from '../../Components/Client/ClientNav';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 function Profile(props) {
 
-    
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState({ houseNo: "",location: "",pin: "", });
+    const [user, setUser] = useState(null);
     const navigate = useNavigate()
     
     useEffect(() => {
         if (!localStorage.getItem("user_token")) {
             //if already logged in
-            navigate('/')
+            navigate('/login')
         }
     }, [navigate])
     const currentUser = useSelector((state) => state.user.currentUser);
-    // if(currentUser){
-        //     console.log(currentUser,'current user');
-    // }
+
+
+    const openModal = () => {
+        setModalIsOpen(true);
+      };
+    
+      const closeModal = () => {
+        setModalIsOpen(false);
+      };
+    
+      const handleAddressSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const data = {
+            phoneNo: phone,
+            address: { ...address },
+            userId:user?._id
+          };
+          const response = await axios.put('/user/add-address', data);
+          console.log(response,"from the client profile");
+          setPhone("");
+          setAddress({ houseNo: "", location: "", pin: "" });
+          closeModal();
+        }catch (error) {
+          console.log("Error adding address:", error);
+        }
+      }
+    
+      const handleAddressChange = (e) => {
+        setAddress({
+          ...address,
+          [e.target.name]: e.target.value,
+        });
+      };
     return (
         <>
             <ClientNav />
             <div className="flex items-center justify-center h-screen">
-                <div className="bg-[#0D0E26] flex flex-col items-center justify-center rounded-3xl shadow-2xl p-8 w-96">
-                    <div className="bg-purple-900 rounded-full overflow-hidden w-24 h-24 mb-4">
-                        <img
-                            src="https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg" 
-                            alt="Profile"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="flex flex-col items-center ">
-                        <input
-                            type="text"
-                            readOnly
-                            value={currentUser.data.username}
-                            className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64"
-                        />
-                        <input
-                            type="email"
-                            readOnly
-                            value={currentUser.data.email}
-                            className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522(257) 563-7401"
-                            className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64"
-                        />
-                        <div className="flex justify-center align-middle">
-                            <button className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-20">Save</button>
-                            <button className="bg-purple-900 text-white border-b-2 ml-1 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-50">Transaction History</button>
-                        </div>
-                    </div>
+        <div className="flex flex-col items-center bg-gradient-to-br from-purple-950 to-purple-950 rounded-3xl shadow-2xl p-8 h-[420px] w-96">
+          <div className="bg-purple-900 rounded-full overflow-hidden w-24 h-24 mb-4">
+            <img
+              src="/profileIcon.png"
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+
+
+  {user ? (
+    <>
+      <div className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64">{user.username}</div>
+      <div className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64">{user.email}</div>
+      <div className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64">
+  {user.address? `${user.address.houseNo}, ${user.address.location}, ${user.address.pin}` : "No address available"}
+</div>
+    </>
+  ) : (
+    <>
+      <div className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64">username</div>
+      <div className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64">email</div>
+      <div className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-64">location,address,pin</div>
+    </>
+  )}
+</div>
+
+
+          <div>
+            <button onClick={openModal} className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 rounded-md focus:outline-none w-32">AddAddress</button>
+            {/* Modal component */}
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Add Post Modal" className="absolute inset-0 flex items-center justify-center" overlayClassName="fixed inset-0 bg-gray-700 bg-opacity-75" closeTimeoutMS={200}>
+              <div className="bg-white rounded-lg p-8 max-w-lg w-full">
+                <h5>Phone No:</h5>
+                <input
+                  name="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  className="border py-2 px-4 mb-4 w-full"
+                  placeholder="90XXXXXXXX"
+                  type="text"
+                />
+                <h5>Address</h5>
+                <input
+                  name="houseNo"
+                  value={address.houseNo}
+                  onChange={handleAddressChange}
+                  className="border py-2 px-4 mb-4 w-full h-8"
+                  placeholder="House No"
+                  type="text"
+                />
+                <input
+                  name="location"
+                  value={address.location}
+                  onChange={handleAddressChange}
+                  className="border py-2 px-4 mb-4 w-full h-8"
+                  placeholder="Location"
+                  type="text"
+                />
+                <input
+                  name="pin"
+                  value={address.pin}
+                  onChange={handleAddressChange}
+                  className="border py-2 px-4 mb-4 w-full h-8"
+                  placeholder="Pin"
+                  type="text"
+                />
+
+                <div className="flex justify-between">
+                  <button onClick={handleAddressSubmit} className="bg-slate-600 text-white py-2 px-6 rounded-lg mr-4">Save</button>
+                  <button onClick={closeModal} className="bg-red-800 text-white py-2 px-6 rounded-lg">Close Modal</button>
                 </div>
-            </div>
+              </div>
+            </Modal>
+
+            <button className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 ml-4 rounded-md focus:outline-none w-28">Transaction</button>
+          </div>
+            {/* <button className="bg-purple-900 text-white border-b-2 border-purple-700 px-4 py-2 mb-2 ml-4 rounded-md focus:outline-none w-28">Save</button> */}
+        </div>
+      </div>
         </>
     );
 }
