e9b800bcbc4f6a20d720f0882917d2833da4e78f
aswin krishna <aswinkrishna33716@gmail.com>
2024-06-04 23:44:44 +0530

    updates in painter profile in user side

--- src/Pages/Users/UserPainterProfile.jsx
+++ src/Pages/Users/UserPainterProfile.jsx
@@ -1,34 +1,48 @@
 import React, { useRef, useState,useEffect } from "react";
 import Modal from 'react-modal'
+import { FiMoreHorizontal, FiHeart, FiSend } from 'react-icons/fi'; // Importing additional icons
 import PainterNav from "../../Components/Painter/PainterNav";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { isValidImageType } from "../../services/validations";
 import toast, { Toaster } from "react-hot-toast";
 import uploadImageToFirebase from "../../services/Firebase/imageUploader";
-import { saveProfilepic, uploadPost } from "../../api/painterApi";
+import { saveProfilepic, uploadPost ,updateDetails } from "../../api/painterApi";
+import { DeletePost, getPainterPosts } from "../../api/postApi";
+import { deleteObject, ref } from "firebase/storage";
+import { storage } from "../../services/firebase";
 
 function UserPainterProfile() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [detailsModalIsOpen, setDetailsModalIsOpen] = useState(false);
   const [image, setImage] = useState(undefined)
   const [isImageChanged, setIsImageChanged] = useState(false);
   const [imageUrl,setImageUrl] = useState("")
   const [selectedAvatar, setSelectedAvatar] = useState(null);
   const [previewAvatar, setPreviewAvatar] = useState(null);
   const [description, setDescription] = useState("");
+  const [age, setAge] = useState('');
+  const [experienceYears, setExperienceYears] = useState(''); 
+  const [location, setLocation] = useState(''); 
+  const [phone, setPhone] = useState(''); 
+  const [specialised, setSpecialised] = useState(''); 
+  const [aboutMe, setAboutMe] = useState(''); 
   const descriptionRef = useRef(null);
   const navigate = useNavigate()
   const fileRef = useRef(null)
+  const [ painterPosts, setPainterPosts] = useState([])
+  const [showDeleteButton, setShowDeleteButton] = useState(null);
 
 
   useEffect(() => {
     if (!localStorage.getItem("painter_token")) {
         //if already logged in
-        navigate('/login')
+        navigate('/painter/login')
     }
 }, [navigate])
 
 const currentpainter = useSelector((state) => state.painter.currentUser)
+const id = currentpainter.user._id
 console.log(currentpainter,'currentpainter');
 
   const openModal = () => {
@@ -39,6 +53,30 @@ console.log(currentpainter,'currentpainter');
     setModalIsOpen(false);
   };
 
+  const openDetailsModal = () => {
+    setDetailsModalIsOpen(true);
+  };
+
+  const closeDetailsModal = () => {
+    setDetailsModalIsOpen(false);
+  };
+
+
+  const fetchPainterPosts = async (id) => {
+    try {
+      const posts = await getPainterPosts(id);
+      console.log(posts,'postsssssssss');
+      if (posts) {
+        setPainterPosts(posts.data.posts);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() =>{
+    fetchPainterPosts(id)
+  },[])
 
   const  handleFileChange = (e) =>{
     if(e.target.files && e.target.files[0]){
@@ -135,6 +173,63 @@ console.log(currentpainter,'currentpainter');
         console.log("NO  image selected");
       }
   }
+
+  const handleDetailsSubmit = async (e) => {
+    e.preventDefault()
+
+    const details = {
+      age,
+      experienceYears,
+      location,
+      phone,
+      specialised: specialised.split(","),
+      aboutMe,
+    };
+
+    try {
+      await updateDetails(id,details)
+      if(updateDetails){
+        toast.success("Details updated successfully");
+        setAge('')
+        setExperienceYears('')
+        setLocation('')
+        setPhone('')
+        setSpecialised('')
+        setAboutMe('')
+        closeDetailsModal();
+      }
+    } catch (error) {
+      toast.error("Failed to update details");
+      console.error("Error updating details:", error);
+    }
+  }
+
+
+  const toggleDeleteButton = (postId) => {
+    setShowDeleteButton(!showDeleteButton);
+  };
+
+  const deletePost = async (id) =>{
+    const dlt = ref(storage,painterPosts.media)
+    deleteObject(dlt).then(() => {
+      console.log('deleted success');
+    }).catch((err) =>{
+      console.log(err,'error');
+    })
+    try {
+      const deleted = await DeletePost(id)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleDelete = async (id) =>{
+      if(window.confirm("Are you sure you want to delete this post ?")){
+        await deletePost(id)
+      }
+  }
+
+
   return (
     <>
       <PainterNav/>
@@ -156,22 +251,29 @@ console.log(currentpainter,'currentpainter');
                     onClick={() => fileRef.current.click()}
                   />
                   <h1 className="text-xl font-bold">{currentpainter.user.username}</h1>
-                  <p className="text-gray-700">Professional Painter</p>
-                  <p className="text-gray-700">Location:Kerala ,Calicut</p>
-                  <p className="text-gray-700">Phone:2345362346345</p>
+                  <p className="text-gray-700 placeholder:Age ">Age:{currentpainter.user.age ? currentpainter.user.age : 'Add your Age'}</p>
+                  <p className="text-gray-700 placeholder:location ">Location:{currentpainter.user.location ? currentpainter.user.location : ' Add your Location'}</p>
+                  <p className="text-gray-700 ">Phone:{currentpainter.user.phone ? currentpainter.user.phone : ' Add your phone number here'}</p>
+                  
                   <div className="mt-6 flex flex-wrap gap-4 justify-center">
-                    <a
-                      href="#"
-                      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                  {/* Conditional rendering for Add Post button */}
+                  {!modalIsOpen && (
+                  <div className="relative inline-block  m-2">
+                    {/* Button to open modal */}
+                    <button
+                      className="border-transparent relative z-10 py-2 px-4 text-white font-bold text-lg rounded-[30px] cursor-pointer focus:outline-none bg-gradient-to-r from-blue-900 to-indigo-700"
+                      onClick={openModal}
                     >
-                      Follow
-                    </a>
-                    <a
-                      href="#"
-                      className="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded"
+                      Add Post
+                    </button>
+                    <button
+                      className="border-transparent relative z-10 py-2 px-4 text-white font-bold text-lg rounded-[30px] cursor-pointer focus:outline-none bg-gradient-to-r from-blue-900 to-indigo-700"
+                      onClick={openDetailsModal}
                     >
-                      Message
-                    </a>
+                      Details
+                    </button>
+                  </div>
+                )}
                   </div>
                 </div>
                 <hr className="my-6 border-t border-gray-300" />
@@ -180,12 +282,9 @@ console.log(currentpainter,'currentpainter');
                     specialised :
                   </span>
                   <ul className="flex ">
-                    <li className="mb-2 ml-4">#interior</li>
-                    <li className="mb-2 ml-4">#interior</li>
-                    <li className="mb-2 ml-4">#interior</li>
-                    <li className="mb-2 ml-4">#interior</li>
-                    <li className="mb-2 ml-4">#interior</li>
-                    <li className="mb-2 ml-4">#interior</li>
+                    {currentpainter.user.specialised.map((speciality ,index) => (
+                      <li key= {index} className="mb-2 ml-4">{speciality}</li>
+                    ))}
                   </ul>
                 </div>
               </div>
@@ -206,6 +305,7 @@ console.log(currentpainter,'currentpainter');
               faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam
               erat volutpat. Nulla vulputate pharetra tellus, in luctus risus
               rhoncus id.
+              {/* {currentpainter.user.aboutMe} */}
             </p>
 
             <h2 className="text-xl font-bold mt-6 mb-4">Experience</h2>
@@ -294,25 +394,8 @@ console.log(currentpainter,'currentpainter');
           </div>
         </div>
 
-        {/* My posts  */} 
-
-        <div className=" min-w-[90rem] max-auto rounded-2xl bg-[#0D0E26] min-h-[30rem]">
-                              {/* Conditional rendering for Add Post button */}
-                              {!modalIsOpen && (
-                  <div className="relative inline-block  m-2">
-                    {/* Button to open modal */}
-                    <button
-                      className="border-transparent relative z-10 py-2 px-3 text-white font-bold text-lg rounded-[30px] cursor-pointer focus:outline-none bg-gradient-to-r from-blue-900 to-indigo-700"
-                      onClick={openModal}
-                    >
-                      Add Post
-                    </button>
-                  </div>
-                )}
-
-
-                  {/* Modal component */}
-                  <Modal
+            {/* Modal component */}
+            <Modal
                   isOpen={modalIsOpen}
                   onRequestClose={closeModal}
                   contentLabel="Add Post Modal"
@@ -361,7 +444,144 @@ console.log(currentpainter,'currentpainter');
                     </div>
                   </div>
                 </Modal>
+
+                  {/* Add Details Modal */}
+                  <Modal
+                  isOpen={detailsModalIsOpen}
+                  onRequestClose={closeDetailsModal}
+                  contentLabel="Add/Edit Details Modal"
+                  className="absolute inset-0 flex items-center justify-center"
+                  overlayClassName="fixed inset-0 bg-gray-700 bg-opacity-75"
+                  closeTimeoutMS={200}
+                >
+                  <div className="bg-white rounded-lg p-8 max-w-lg w-full">
+                    <h2 className="text-2xl font-bold mb-4">
+                      {detailsModalIsOpen ? "Edit Details" : "Add Details"}
+                    </h2>
+                    <form onSubmit={handleDetailsSubmit}>
+                      <label htmlFor="age">Age</label>
+                      <input
+                        className="border py-2 px-4 mb-4 w-full"
+                        type="number"
+                        id="age"
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}
+                      />
+                      <label htmlFor="experienceYears">Experience Years</label>
+                      <input
+                        className="border py-2 px-4 mb-4 w-full"
+                        type="number"
+                        id="experienceYears"
+                        value={experienceYears}
+                        onChange={(e) => setExperienceYears(e.target.value)}
+                      />
+                      <label htmlFor="location">Location</label>
+                      <input
+                        className="border py-2 px-4 mb-4 w-full"
+                        type="text"
+                        id="location"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                      />
+                      <label htmlFor="phone">Phone</label>
+                      <input
+                        className="border py-2 px-4 mb-4 w-full"
+                        type="text"
+                        id="phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                      <label htmlFor="specialised">Specialised (comma separated)</label>
+                      <input
+                        className="border py-2 px-4 mb-4 w-full"
+                        type="text"
+                        id="specialised"
+                        value={specialised}
+                        onChange={(e) => setSpecialised(e.target.value)}
+                      />
+                      <label htmlFor="aboutMe">About Me</label>
+                      <textarea
+                        className="border py-2 px-4 mb-4 w-full"
+                        id="aboutMe"
+                        value={aboutMe}
+                        onChange={(e) => setAboutMe(e.target.value)}
+                      />
+                      <div className="flex justify-between">
+                        <button
+                          type="submit"
+                          className="bg-blue-800 text-white py-2 px-6 rounded-lg mr-4"
+                        >
+                          Save
+                        </button>
+                        <button
+                          type="button"
+                          onClick={closeDetailsModal}
+                          className="bg-red-600 text-white py-2 px-6 rounded-lg"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </Modal>
+
+                {/* My posts  */} 
+                {painterPosts.map((post) => (
+               <div className=" min-w-[90rem] max-auto rounded-2xl p-5 bg-[#0D0E26] min-h-[30rem]">
+           
+      <div key={post._id} className="flex flex-col items-start mb-4 p-2">
+        <div className="flex items-center bg-gray-800 rounded-2xl mb-2 justify-between w-full h-16">
+          <div className="flex items-center">
+            <div className="rounded-full overflow-hidden w-14 h-14 m-2">
+              <img
+                src={post.painterId.profile || "https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg"}
+                alt="Profile"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-white font-semibold">{post.painterId.username}</p>
+          </div>
+          <div className="flex items-center">
+            <FiMoreHorizontal className="text-white cursor-pointer" onClick={() => toggleDeleteButton(post._id)} />
+               {showDeleteButton && (
+            <div className=" top-8 right-0 bg-gray-800 text-white rounded-md shadow-lg">
+              <button className="text-white" onClick={ () => handleDelete(post._id)}>Delete</button>
+            </div>
+            )}
+          </div>
         </div>
+        {/* Post content */}
+        <p className="text-white mb-2">{post.description}</p>
+        {post.media && <img src={post.media} alt="Post Media" className="w-full h-[27rem] rounded-xl object-cover" />}
+        
+        {/* Like button and comment box */}
+        <div className="flex justify-between w-full mt-2">
+          <button className="flex items-center text-white">
+            <FiHeart className="mr-2" /> Like
+          </button>
+          <div className="flex flex-col w-52 ">
+            <div className="flex items-center justify-between border-b-2 rounded-md">
+              <input 
+                type="text" 
+                placeholder="Add a comment..." 
+                className="bg-transparent text-white flex-1 w-8 p-1 focus:outline-none"
+              />
+              <button className="text-white w-6 ">
+                <FiSend />
+              </button>
+            </div>
+            {/* Display comments */}
+            {/* <div className="mt-2 bg-gray-800 p-2 rounded-lg">
+              {post.comments && post.comments.map((comment, index) => (
+                <p key={index} className="text-white mb-1">{comment}</p>
+              ))}
+            </div> */}
+          </div>
+        </div>
+      </div>
+              
+        </div>
+      ))}
       </div>
     </>
   );
