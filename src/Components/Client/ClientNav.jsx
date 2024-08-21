6c3014419184403517ac8d1f76e98f447d63e695
aswin krishna <aswinkrishna33716@gmail.com>
2024-08-21 23:24:48 +0530

    search for painters

--- src/Components/Client/ClientNav.jsx
+++ src/Components/Client/ClientNav.jsx
@@ -15,12 +15,14 @@ import { Logout, searchPainter } from '../../api/userApi';
 function ClientNav(props) {
 
   const Navigate = useNavigate()
-  const [searchQuery,setSearchQuery] = useState('') 
+  const [searchQuery,setSearchQuery] = useState('')
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearch =  async () => {
     try {
       const response = await searchPainter(searchQuery)
-      // setPosts(response.data.filteredPosts )
+        setSearchResults(response.data.data)
+      setSearchQuery('')
     } catch (error) {
       console.log(error);
     }
@@ -38,74 +40,99 @@ function ClientNav(props) {
 
 
     return (
-        <Navbar
-      variant="gradient"
-      color=""
-      className=" mx-auto max-w-full w-full px-4 py-3 rounded-none h-[60px] fixed z-50 bg-black"
-    >
-      <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
-        <Typography
-          as="a"
-          href="#"
-          variant="h6"
-          className="mr-4 ml-2 cursor-pointer py-1.5"
-        >
-          Paintcont
-        </Typography>
-
-        <div className="flex md:flex flex-wrap gap-4 sm:gap-14 md:mr-4 sm:mr-4">
-          {/* Convert text into links */}
-          <span onClick={() => Navigate('/')} className="text-white cursor-pointer">
-            Home
-          </span>
-          <span onClick={() => Navigate('/chat')} className="text-white cursor-pointer">
-            Chat
-          </span>
-          <span onClick={() => Navigate('/about')} className="text-white cursor-pointer">
-            About Us
-          </span>
-          <span onClick={() => Navigate('/contact')} className="text-white cursor-pointer">
-            Contact Us
-          </span>
-
-        </div>
-
-        <div className="relative flex w-full gap-2 md:w-max">
-          <Input
-            type="search"
-            color="white"
-            label='Type here...'
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="pr-10 bg-transparent border-b border-white focus:outline-none"
-            containerProps={{
-              className: "min-w-[288px]",
-            }}
-          />
-          <Button
-            size="sm"
-            color="white"
-            className="!absolute right-1 top-1 rounded"
-            onClick={handleSearch}
+      <Navbar
+        variant="gradient"
+        color=""
+        className=" mx-auto max-w-full w-full px-4 py-3 rounded-none h-[60px] fixed z-50 bg-black"
+      >
+        <div className="flex flex-wrap items-center justify-between gap-y-4 text-white">
+          <Typography
+            as="a"
+            href="#"
+            variant="h6"
+            className="mr-4 ml-2 cursor-pointer py-1.5"
           >
-            Search
-          </Button>
+            Paintcont
+          </Typography>
+
+          <div className="flex md:flex flex-wrap gap-4 sm:gap-14 md:mr-4 sm:mr-4">
+            {/* Convert text into links */}
+            <span
+              onClick={() => Navigate("/")}
+              className="text-white cursor-pointer"
+            >
+              Home
+            </span>
+            <span
+              onClick={() => Navigate("/chat")}
+              className="text-white cursor-pointer"
+            >
+              Chat
+            </span>
+            <span
+              onClick={() => Navigate("/about")}
+              className="text-white cursor-pointer"
+            >
+              About Us
+            </span>
+            <span
+              onClick={() => Navigate("/contact")}
+              className="text-white cursor-pointer"
+            >
+              Contact Us
+            </span>
+          </div>
+
+          <div className="relative flex w-full gap-2 md:w-max">
+            <Input
+              type="search"
+              color="white"
+              label="Type here..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="pr-10 bg-transparent border-b border-white focus:outline-none"
+              containerProps={{
+                className: "min-w-[288px]",
+              }}
+            />
+            <Button
+              size="sm"
+              color="white"
+              className="!absolute right-1 top-1 rounded"
+              onClick={handleSearch}
+            >
+              Search
+            </Button>
+            {searchResults.length > 0 && (
+              <div className="absolute top-[60px] w-full bg-white text-black p-2 rounded shadow-md">
+                {searchResults.map((painter) => (
+                  <div
+                    key={painter._id}
+                    className="p-2 border-b cursor-pointer border-gray-300" onClick={() => Navigate(`/painterprofile/${painter._id}`)}
+                  >
+                    
+                    {painter.username}
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
 
           {/* Adding profile and logout icons */}
           <div className="flex items-center gap-3">
-          <span onClick={() => Navigate("/profile")} className="cursor-pointer">
-          <RiUserLine className="text-lg" />
-          </span>
+            <span
+              onClick={() => Navigate("/profile")}
+              className="cursor-pointer"
+            >
+              <RiUserLine className="text-lg" />
+            </span>
 
             <button onClick={logout}>
               <RiLogoutCircleLine className="text-lg" />
-
             </button>
-            
           </div>
-      </div>
-    </Navbar>
+        </div>
+      </Navbar>
     );
 }
 
